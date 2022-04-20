eb454f4cfb3de1ca2a7b35e980f19f6d1edcbb6c
david-garza <locodave@gmail.com>
2022-04-20 15:34:59 -0500

    centered and cleared map

--- mapping_geojson/static/js/logic.js
+++ mapping_geojson/static/js/logic.js
@@ -1,25 +1,5 @@
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
-
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+let map = L.map('mapid').setView([30,30], 2);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -31,15 +11,3 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
-
-// Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport, {
-    onEachFeature: function(feature, layer) {
-        console.log(layer);
-        layer.bindPopup("<h2>"+"Airport Code: "
-        +feature.properties.faa+"</h2>"
-        +"<hr>"
-        +"<h2>"+ "Airport Name: " + feature.properties.name +"</h2>");    
-    }
-
-  }).addTo(map);
