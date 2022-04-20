7250a285cec84034648dc042d349191132edd5e5
david-garza <locodave@gmail.com>
2022-04-20 11:55:25 -0500

    skill drill 1

--- mapping_geojson/static/js/logic.js
+++ mapping_geojson/static/js/logic.js
@@ -22,9 +22,10 @@ let sanFranAirport =
 let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
+    id: "navigation-night-v1",
     accessToken: API_KEY
 });
 
@@ -37,7 +38,7 @@ L.geoJSON(sanFranAirport, {
     pointToLayer: function(feature, latlng) {
       console.log(feature);
       return L.marker(latlng)
-      .bindPopup("<h2>" + feature.properties.city + "</h2>");
+      .bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h3>" + feature.properties.city + ", " + feature.properties.country+"</h3>");
     }
 
   }).addTo(map);
