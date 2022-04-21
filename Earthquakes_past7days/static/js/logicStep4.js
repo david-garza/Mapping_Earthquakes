9b68c467bc0b6eed0497e838e8e8e1723d13a5f5
david-garza <locodave@gmail.com>
2022-04-21 15:20:20 -0500

    added earthquakes to the control box

--- Earthquakes_past7days/static/js/logicStep4.js
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -18,6 +18,15 @@ let baseMaps = {
     "Satellite": satelliteStreets
   };
 
+// Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
+
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+    Earthquakes: earthquakes
+  };
+
 // Create the map object with a center and zoom level.
 let map = L.map('mapid',{
     center: [39.5, -98.5],
@@ -26,7 +35,7 @@ let map = L.map('mapid',{
 })
 
 // Pass to control to map
-L.control.layers(baseMaps).addTo(map);
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // Earthquake data URL
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
@@ -93,5 +102,8 @@ d3.json(url).then(function(data) {
         onEachFeature: function(feature, layer) {
             layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
         }
-    }).addTo(map);
+    }).addTo(earthquakes);
 });
+
+// Then add the layer earthquakes to map
+earthquakes.addTo(map);
