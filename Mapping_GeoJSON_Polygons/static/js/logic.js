f7bbb8cfadf613b83614576f9ea41edada87d811
david-garza <locodave@gmail.com>
2022-04-21 12:48:40 -0500

    adjusted map around toronto

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -7,41 +7,25 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 
 // We create the tile layer that will be the background of our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: "dark-v10",
+    id: "satellite-streets-v11",
     accessToken: API_KEY
 });
 
 // Create a base layer that holds both maps.
 let baseMaps = {
   Street: streets,
-  Dark: dark
+  Dark: satelliteStreets
 };
 
 // Create the map object with a center and zoom level.
 let map = L.map('mapid',{
-  center: [40.7, -94.5],
-  zoom: 2,
+  center: [43.7, -79.3],
+  zoom: 11,
   layers: [streets]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
-
-// Aiport JSON data
-let airportData = "https://raw.githubusercontent.com/david-garza/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
-
-// Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
-    console.log(data);
-    // Creating a GeoJSON layer with the retrieved data.
-    L.geoJSON(data, {
-      onEachFeature: (feature, layer) =>{
-        layer.bindPopup("<h2>"+"Airport Code: "+feature.properties.faa+"</h2>"
-        +"<hr>"
-        +"<h3>"+"Airport Name: "+feature.properties.name+"</h3>");
-      }
-    }).addTo(map);
-});
